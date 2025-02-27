ca72a828a20e60bddf22bbf862c602d4a25d7411
rizkyaqil04 <166620425+rizkyaqil04@users.noreply.github.com>
2025-02-27 16:09:53 +0000

    edit bert.py

--- src/training/bert.py
+++ src/training/bert.py
@@ -75,7 +75,7 @@ def compute_topics_with_bertopic(papers, save_model=True):
             umap_model=umap_model,
             hdbscan_model=hdbscan_model,
             vectorizer_model=None,
-            embedding_model="all-MiniLM-L6-v2"
+            embedding_model=model
         )
         
         topic_model.fit_transform(texts, embeddings)
