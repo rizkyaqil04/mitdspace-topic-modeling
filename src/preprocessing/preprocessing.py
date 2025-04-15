e3d7c218e827c29e8e9eab6cc9320ab42fae531a
rizkyaqil04 <166620425+rizkyaqil04@users.noreply.github.com>
2025-04-15 06:09:07 +0000

    LK04

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -9,6 +9,7 @@ from src.utils.logger import setup_logger
 # Paths for storing preprocessing results
 SCRAPED_DATA_PATH = Path("data/raw/mit_scraped_500.json")
 PREPROCESSED_DATA_PATH = Path("data/processed/data_preprocessed.json")
+MODEL_LOCAL_PATH = "models/all-MiniLM-L6-v2"
 EMBEDDING_PATH = Path("data/processed/embeddings.npy")
 TFIDF_FEATURES_PATH = Path("data/processed/tfidf_features.json")
 
@@ -90,7 +91,15 @@ def compute_embeddings(texts, save_path=EMBEDDING_PATH):
     #     return np.load(save_path)
     
     logging.info("Computing embeddings using SentenceTransformer.")
+    
     model = SentenceTransformer("all-MiniLM-L6-v2")
+    
+    if os.path.exists(MODEL_LOCAL_PATH):
+        model = SentenceTransformer(MODEL_LOCAL_PATH)  # Gunakan model dari lokal
+    else:
+        model = SentenceTransformer("all-MiniLM-L6-v2")  # Unduh dari Hugging Face
+        model.save(MODEL_LOCAL_PATH)  # Simpan untuk penggunaan selanjutnya
+    
     embeddings = model.encode(texts, batch_size=32, show_progress_bar=True, normalize_embeddings=True)
     np.save(save_path, embeddings)
     logging.info(f"Embeddings saved at {save_path}")
