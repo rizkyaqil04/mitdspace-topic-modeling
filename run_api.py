ac6119e9d6d0f6893841f1db85b0aec9bec3b7ae
kroja-nk <rizkyaqil04@gmail.com>
2025-04-18 07:47:29 +0700

    Pemformatan Dataset

--- run_api.py
+++ run_api.py
@@ -1,18 +1,5 @@
 import uvicorn
 from fastapi import FastAPI
-<<<<<<< HEAD
-<<<<<<< HEAD
-from src.api.scrape_svc import app as scraping_app
-from src.api.menampilkan_svc import app as menampilkan_app
-
-app = FastAPI()
-
-# Menyertakan sub-app untuk scraping dan preprocessing
-app.mount("/a", scraping_app)
-app.mount("/b", menampilkan_app)
-=======
-=======
->>>>>>> Aqill's
 from src.api.scrape_svc import router as scraping_router
 from src.api.preprocessing_svc import router as preprocessing_router
 from src.api.menampilkan_svc import router as menampilkan_router
@@ -25,10 +12,6 @@ app.include_router(scraping_router, tags=["Scraping"])
 app.include_router(preprocessing_router, tags=["Preprocessing"])
 app.include_router(menampilkan_router, tags=["Menampilkan"])
 app.include_router(training_router, tags=["Training"])
-<<<<<<< HEAD
->>>>>>> Aqill's
-=======
->>>>>>> Aqill's
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
